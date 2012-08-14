97e77cd0e8d872b5261e387788bb8fd39660db22
chenchiyuan <chenchiyuan03@gmail.com>
2012-08-14 14:24:50 +0800

    seperate client to ribida

--- tag/__init__.py
+++ tag/__init__.py
@@ -1,9 +1,8 @@
-from client import API
 from services.wordseg import BaseSeg as seg
 from sockets.tag_rank import run as run_tag_rank
 from sockets.relations import run as run_relations
 from models import Tag, TagManager
 from utils import TagFileHelper
 
-__all__ = [API, seg, run_tag_rank, run_relations, Tag,
+__all__ = [seg, run_tag_rank, run_relations, Tag,
            TagManager, TagFileHelper]
